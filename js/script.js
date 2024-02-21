00395c2874e7fea6bbdb7f71ef49f091c39967ee
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-02-21 23:11:06 +0100

    [fixed] struttura base js

--- js/script.js
+++ js/script.js
@@ -3,43 +3,54 @@
 
 
 // input 
+
 const name= document.querySelector('.name')
-let km= document.querySelector ('.km')
-let eta= document.querySelector ('.age ')
-
-
+let km= document.querySelector ('.km');
+let eta=document.querySelector ('.age ')
 
 // btn 
 const btn1= document.querySelector('.btn-1')
 const btnReset= document.querySelector('.btn-reset')
 
-const prezzoKm = 0.21
-let costoBiglietto= km * prezzoKm
-let scontoKmMinorenni= prezzoKm*20/100
-let scontoKmOver=prezzoKm*40/100
-let biglittoScontatoMinori=(costoBiglietto-(scontoKmMinorenni*km))
-let fixedBigliettoMinori=biglittoScontatoMinori.toFixed(2)
-let bigliettoScontatoOver=(costoBiglietto-(scontoKmOver*km))
-let fixedBigliettoOver=bigliettoScontatoOver.toFixed(2)
+
+
 
 let messaggio=''
 
 
 btn1.addEventListener('click', function(){
+  let eta=document.querySelector ('.age ')
   
-  if (eta= 'Minorenne'){
-  messaggio= (fixedBigliettoMinori)
+  const prezzoKm = 0.21
+  let costoBiglietto= km.value * prezzoKm
+  console.log(eta.value)
+  console.log('cliccato')
+
+
+  if ( eta.value <= 17 ){
+  console.log (km.value)
+  let scontoKmMinorenni= prezzoKm*20/100
+  let biglittoScontatoMinori=(costoBiglietto-(scontoKmMinorenni*km.value))
+  let fixedBigliettoMinori=biglittoScontatoMinori.toFixed(2)
   console.log(fixedBigliettoMinori)
-}
+  }
 
-else if ( eta= 'over 65'){
-  messaggio= (fixedBigliettoOver)}
-
-else{
-  messaggio=(costoBiglietto)
-}
+  else if ( eta.value>= 65 ){
+  console.log (km.value)
+  
+  let scontoKmOver=prezzoKm*40/100
+  
+  let bigliettoScontatoOver=(costoBiglietto-(scontoKmOver*km.value))
+  let fixedBigliettoOver=bigliettoScontatoOver.toFixed(2)
+  console.log(fixedBigliettoOver)}
+  else{
+   console.log(costoBiglietto)
+  
+  }
 })
 
-
-
-document.getElementById('totale').innerHTML+=messaggio
+btnReset.addEventListener('click',function(){
+  name.value= '';
+  km.value='';
+  eta.value='';
+})
